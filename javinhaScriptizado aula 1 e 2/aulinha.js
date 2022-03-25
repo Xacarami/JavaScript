c95a45b1ef855fce2b40f2feb4369aa6f8e9e47d
Xacarami <57050195+Xacarami@users.noreply.github.com>
2022-03-25 20:30:16 -0300

    Add files via upload

---
+++ javinhaScriptizado aula 1 e 2/aulinha.js
@@ -0,0 +1,44 @@
+        //Strings
+
+//primeira coisa ensinada
+alert("Oia eu aqui")
+
+//variáveis pode ser com var, let e const
+
+let message = "Olá mundo!"
+//Se for let, então pode alterar
+//Se for const, então não pode
+
+message = "Opa"
+
+console.log(message.length)
+
+alert(message)
+
+const firstname = "Gabriel"
+const lastname = "Plautz"
+
+console.log(`Meu nome é ${firstname.toUpperCase()} ${lastname.toLowerCase()}`)
+
+const names = "Gabriel,Lucas,Pablo"
+
+//Lista
+
+console.log(names.split(","));
+
+            //Numbers
+
+const number = 5;
+
+console.log(number / 2);
+
+        //Booleanos
+//false / true - Null = (vazio) e Undefined
+
+const x = null; //vazio
+const y = undefined; //indefinido
+
+                //Listas e Objetos
+
+const list = [1, 2, 3, 4, 5, 6, 7]; //pode guardar vários valores
+const object = {name: "Gabriel"}; //pode guardar propriedades e valores
